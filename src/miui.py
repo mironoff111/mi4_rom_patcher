625f433ffeb73bae8eef5b17bf1eaf6195e710a8
Grigory E <miron36357@yandex.ru>
2015-09-08 17:27:24 +0300

    новые фичи+убран бинарник

--- src/miui.py
+++ src/miui.py
@@ -1,3 +1,4 @@
+#Oracle JRE REQUIRED!!!!
 import os
 import sys
 import zipfile
@@ -9,17 +10,17 @@ def unzip(source_filename, dest_dir):
     with zipfile.ZipFile(source_filename) as zf:
         zf.extractall(dest_dir)
 		
-def zipdir(path, ziph):
+def zipdir(path, ziph, secret):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            s=path_rom_dir+"/miui_rom_tmp"
+            s=path_rom_dir+secret
             s = s.replace("\\", "\\\\")
             zippath = os.path.join(root, file)
             zippath = re.sub(s, "", zippath)
             ziph.write(os.path.join(root, file), zippath)
 
-def ReplaceLineInFile(fileName, sourceText, replaceText):
+def PatchString(fileName, sourceText, replaceText):
     print ('Патчим '+fileName+'\n')
     file = open(fileName, 'r') #Opens the file in read-mode
     text = file.read() #Reads the file and assigns the value to a variable
@@ -36,9 +37,12 @@ print ('---by mironoff (2015)---\n\n------------------------\n')
 path_rom=input('Введите путь до прошивки:\n\n')
 path_rom_dir=os.path.dirname(path_rom)
 if len (path_rom) > 3:
+	#Распаковываем прошивку
 	print ("\nРаспаковка прошивки...\n")	
 	firmpath = path_rom_dir+'/miui_rom_tmp'
 	unzip(path_rom, firmpath)
+	
+	#Удаляем китайщину
 	print ("Немного магии...\n")
 	os.remove(path_rom_dir+'/miui_rom_tmp/system/app/BrowserProviderProxy.apk')
 	os.remove(path_rom_dir+'/miui_rom_tmp/system/app/BugReport.apk')
@@ -81,29 +85,46 @@ if len (path_rom) > 3:
 	os.remove(path_rom_dir+'/miui_rom_tmp/system/xbin/su')
 	os.remove(path_rom_dir+'/miui_rom_tmp/system/lib/egl/libGLES_android.so')	
 	
-	#BLOBS
+	#Блобсы
 	print ("Грузим blobs...\n")
 	url = 'https://github.com/mironoff111/mi4_rom_patcher/blob/master/src/miui_blobs.zip?raw=true'
 	urlretrieve(url, path_rom_dir+'/miui_blobs.zip')
 	unzip(path_rom_dir+'/miui_blobs.zip', path_rom_dir+'/miui_blobs')
 	
+	#SuperSu
 	shutil.copyfile(path_rom_dir+'/miui_blobs/su', path_rom_dir+'/miui_rom_tmp/system/xbin/su')
 	shutil.copyfile(path_rom_dir+'/miui_blobs/Superuser.apk', path_rom_dir+'/miui_rom_tmp/system/app/Superuser.apk')
+	#Xperia Keyboard
 	shutil.copyfile(path_rom_dir+'/miui_blobs/textinput-tng.apk', path_rom_dir+'/miui_rom_tmp/system/app/textinput-tng.apk')
 	shutil.copyfile(path_rom_dir+'/miui_blobs/libswiftkeysdk-java.so', path_rom_dir+'/miui_rom_tmp/system/lib/libswiftkeysdk-java.so')
+	#Google VoiceSearch
 	shutil.copyfile(path_rom_dir+'/miui_blobs/VoiceSearch.apk', path_rom_dir+'/miui_rom_tmp/system/app/VoiceSearch.apk')	
 	
-	ReplaceLineInFile(path_rom_dir+'/miui_rom_tmp/system/etc/gps.conf', 'time.gpsonextra.net', '194.190.168.1')
-	ReplaceLineInFile(path_rom_dir+'/miui_rom_tmp/system/etc/mixer_paths_3_2_forte.xml', '<ctl name="TI PA Gain" value="36" />', '<ctl name="TI PA Gain" value="53" />')
-	ReplaceLineInFile(path_rom_dir+'/miui_rom_tmp/system/lib/egl/egl.cfg', '0 0 android\n', '')
+	#Патч ntp 
+	PatchString(path_rom_dir+'/miui_rom_tmp/system/etc/gps.conf', 'time.gpsonextra.net', '194.190.168.1')
+	#Патч громкости
+	PatchString(path_rom_dir+'/miui_rom_tmp/system/etc/mixer_paths_3_2_forte.xml', '<ctl name="TI PA Gain" value="36" />', '<ctl name="TI PA Gain" value="53" />')
+	#Патч графики
+	PatchString(path_rom_dir+'/miui_rom_tmp/system/lib/egl/egl.cfg', '0 0 android\n', '')
 	
+	#Эффект телевизора
 	unzip(path_rom_dir+'/miui_rom_tmp/system/framework/services.jar', path_rom_dir+'/miui_services_jar')
-	os.system("java -jar "+path_rom_dir+"/miui_blobs/baksmali-2.0.6.jar -o classout/ "+path_rom_dir+'/miui_services_jar/classes.dex')
+	os.system("java -jar "+path_rom_dir+"/miui_blobs/baksmali-2.0.6.jar -o "+path_rom_dir+'/miui_services_out/ '+path_rom_dir+'/miui_services_jar/classes.dex')
+	PatchString(path_rom_dir+'/miui_services_out/com/android/server/power/DisplayPowerController.smali', 'ELECTRON_BEAM_OFF_ANIMATION_DURATION_MILLIS:I = 0x0', 'ELECTRON_BEAM_OFF_ANIMATION_DURATION_MILLIS:I = 0x190')	
+	PatchString(path_rom_dir+'/miui_services_out/com/android/server/power/DisplayPowerController.smali', 'const-wide/16 v1, 0x0', 'const-wide/16 v1, 0x190')
+	#Удаляем старый classes.dex
+	os.remove(path_rom_dir+'/miui_services_jar/classes.dex')
+	#Создаем новый classes.dex
+	os.system("java -Xmx128M -jar "+path_rom_dir+"/miui_blobs/smali-2.0.6.jar "+path_rom_dir+'/miui_services_out/ -o '+path_rom_dir+'/miui_services_jar/classes.dex')
+	#Архивируем
+	zipf = zipfile.ZipFile(path_rom_dir+'/services.zip', 'w')
+	zipdir(path_rom_dir+'/miui_rom_jar', zipf, "/miui_rom_jar")
+	os.remove(path_rom_dir+'/miui_rom_tmp/system/framework/services.jar')	
+	shutil.copyfile(path_rom_dir+'/services.zip', path_rom_dir+'/miui_rom_tmp/system/framework/services.jar')
 	
 	print ("Архивируем...\n")
-	
 	zipf = zipfile.ZipFile(path_rom_dir+'/miui_rom_patched.zip', 'w')
-	zipdir(path_rom_dir+'/miui_rom_tmp', zipf)
+	zipdir(path_rom_dir+'/miui_rom_tmp', zipf, "/miui_rom_tmp")
 	zipf.close()
 	print ("Прибираемся...\n")
 	shutil.rmtree(path_rom_dir+'/miui_rom_tmp')
--- src/pack.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable   
-
-setup(   
-    name = "MIUI_patcher",   
-    version = "1.0",   
-    description = "MIUI v7 Mi3W/4 patcher by mironoff",   
-    executables = [Executable("miui.py")])
